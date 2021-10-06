acea2cfca460afa17ffc93dad7bc98aa40f99ef6
nathanieldcooke <nathanield.cooke@gmail.com>
2021-10-05 23:55:57 -0700

    complete Phase 3, routes

--- twitter-front-endd/src/App.js
+++ twitter-front-endd/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import LoginForm from './components/session/LoginForm';
 import RegistrationForm from './components/session/RegistrationForm';
+import { AuthRoute, ProtectedRoute } from './Routes'
 
 function App() {
   return (
@@ -14,18 +15,20 @@ function App() {
         <NavLink to='/'>Home</NavLink>
         <NavLink to='register'>Register</NavLink>
         <NavLink to='/login'>Login</NavLink>
-
       </nav>
       <Switch>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route path='/register'>
-          <RegistrationForm />
-        </Route>
-        <Route path='/login'>
-          <LoginForm/>
-        </Route>
+        {/* <Route exact path='/'> */}
+        <ProtectedRoute exact path="/" component={Home} />
+          {/* <Home/> */}
+        {/* </Route> */}
+        {/* <Route path='/register'> */}
+        <AuthRoute path="/register" component={RegistrationForm} />
+          {/* <RegistrationForm /> */}
+        {/* </Route> */}
+        {/* <Route path='/login'> */}
+          <AuthRoute path="/login" component={LoginForm} />
+          {/* <LoginForm/> */}
+        {/* </Route> */}
         <Route path='/users/:userId'>
           <Profile />
         </Route>
